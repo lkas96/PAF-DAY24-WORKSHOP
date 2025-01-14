6e0c4f258b783f52eb04a53de5131828c220feff
lkas96 <kwek_lawson@hotmail.com>
2025-01-14 14:34:23 +0800

    Springboot template

---
+++ src/main/java/vttp_b5_paf_day24_workshop/PAF/Day/_4/Workshop/PafDay24WorkshopApplication.java
@@ -0,0 +1,13 @@
+package vttp_b5_paf_day24_workshop.PAF.Day._4.Workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafDay24WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafDay24WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp_b5_paf_day24_workshop/PAF/Day/_4/Workshop/PafDay24WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp_b5_paf_day24_workshop.PAF.Day._4.Workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafDay24WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
